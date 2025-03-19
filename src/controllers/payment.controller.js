74670904a95a6f3f5ab73337a2758f2951d3b106
Lanna Gabriela <166149007+LannaViana@users.noreply.github.com>
2025-03-19 13:56:45 +0000

    Update Payment Controller

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,12 +1,10 @@
 import {z} from "zod";
 
 const PaymentSchema = z.object({
-    data: z.string(),
-    numerorecibo: z.number().int().positive({message: "Número do recibo inválido"}),
-    usuarioID: z.number().int().positive({message: "Id do usuário inválido"}),
-    valor: z.number().positive({message: "Valor inválido"}),
-    observacao: z.string().max(100,{message: "Observação inválida"})
-
+    data: z.string().datetime(),
+   valor: z.number().positive(),
+   numero: z.number().int().positive(),
+   observacao: z.string().optional(),
 });
 
 const PaymentController = {
@@ -30,8 +28,33 @@ const PaymentController = {
             }
             res.status(500).json({ message: error.message });
         }
-    }
+    },
+
+    async updatePayment(req, res) {
+        try {
+            const {id} = req.params;
+            const {valor, numero, data, observacao} = req.body;
+            PaymentSchema.parse({data, numero, valor, observacao});
+            res.status(200).json({ message: `Payment updated successfully`, data: {id, valor, numero, data, observacao} });
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                res.status(400).json({message: "Validation error", details: error.errors});
+            }
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    async deletePayment(req, res) {
+        try {
+            const {id} = req.params;
+            // Logic to delete payment by id
+            return res.status(200).json({ message: 'Payment deleted successfully' });
+        } catch (error) {
+            return res.status(500).json({ message: 'Internal server error' });
+        }
+    },
 
 }
+
 
 export default PaymentController;
